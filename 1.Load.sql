334df682e14ffa7ce206ad1db0ebb1d3e55eb029
George Fraser <george@fivetran.com>
2020-04-13 11:45:51 -0700

    First cut at state-level

--- 1.Load.sql
+++ 1.Load.sql
@@ -1,26 +1,9 @@
--- Download the data for all seasons by HHS region from FluView interactive.
--- Delete the header lines from the CSVs.
--- 
--- Download case data from https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv
--- Remove header, save as us_states.csv
--- 
--- Make hhs_regions.csv out of HHS region definitions from https://www.marchofdimes.org/peristats/popup.aspx?width=50%&s=faq&reg=&top=&id=20
---
--- Download state populations from https://www2.census.gov/programs-surveys/popest/tables/2010-2019/state/totals/nst-est2019-01.xlsx
--- Select 2019 estimate and save as census_population.csv
-
-drop table if exists ilinet_visits;
-drop table if exists clinical_labs;
-drop table if exists public_health_labs;
-drop table if exists combined_labs;
-drop table if exists hhs_regions;
-drop table if exists census_population;
-drop table if exists nyt_cases;
 
 create table ilinet_visits (
     -- REGION TYPE
     region_type text,
     -- REGION
+    -- Note that "New York City" is a separate region.
     region text,
     -- YEAR
     year integer,
@@ -55,6 +38,7 @@ create table clinical_labs (
     -- REGION TYPE
     region_type text,
     -- REGION
+    -- Note that "New York City" is a separate region, but it always reports null.
     region text,
     -- YEAR
     year integer,
@@ -75,38 +59,11 @@ create table clinical_labs (
     primary key (region, year, week)
 );
 
-create table public_health_labs (
-    -- REGION TYPE
-    region_type text,
-    -- REGION
-    region text,
-    -- YEAR
-    year integer,
-    -- WEEK
-    week integer,
-    -- TOTAL SPECIMENS
-    total_specimens integer,
-    -- A (2009 H1N1)
-    a_h1n1 integer,
-    -- A (H3)
-    a_h3 integer,
-    -- A (Subtyping not Performed)
-    a_no_subtype integer,
-    -- B
-    b integer,
-    -- BVic
-    b_vic integer,
-    -- BYam
-    b_yam integer,
-    -- H3N2v
-    h3n2v integer,
-    primary key (region, year, week)
-);
-
 create table combined_labs (
     -- REGION TYPE
     region_type text,
     -- REGION
+    -- Note that "New York City" is a separate region.
     region text,
     -- YEAR
     year integer,
@@ -133,32 +90,16 @@ create table combined_labs (
     primary key (region, year, week)
 );
 
-create table hhs_regions (
-    region text,
-    state text,
-    primary key (region, state)
-);
-
-create table census_population (
-    state text, 
-    population integer,
-    primary key (state)
-);
-
 create table nyt_cases (
-    d date,
+    date date,
     state text,
     fips integer,
     cases integer,
     deaths integer,
-    primary key (d, state)
+    primary key (date, state)
 );
 
-.mode csv
-.import ILINet.csv ilinet_visits
-.import WHO_NREVSS_Clinical_Labs.csv clinical_labs
-.import WHO_NREVSS_Public_Health_Labs.csv public_health_labs
-.import WHO_NREVSS_Combined_prior_to_2015_16.csv combined_labs
-.import hhs_regions.csv hhs_regions
-.import census_population.csv census_population
-.import us_states.csv nyt_cases
+copy ilinet_visits from './data/ILINet.csv' with header null 'X';
+copy clinical_labs from './data/WHO_NREVSS_Clinical_Labs.csv' with header null 'X';
+copy combined_labs from './data/WHO_NREVSS_Combined_prior_to_2015_16.csv' with header null 'X';
+copy nyt_cases from './data/NYT_Cases.csv' with header;
---
+++ 2.Transform.sql
@@ -0,0 +1,65 @@
+drop table if exists cdc_dates;
+drop table if exists combined;
+
+create table cdc_dates as
+with distinct_weeks as (select distinct year, week, cast(dense_rank() over (order by year, week) as integer) as week_number from ilinet_visits)
+select year, week, date '2020-01-04' + 7 * (week_number - (select week_number from distinct_weeks where year = 2020 and week = 1)) as date 
+from distinct_weeks
+order by year, week;
+
+create table combined as
+with tests as (
+    select 
+        date, 
+        region as state, 
+        -- Most states report nulls instead of 0 when they have no specimens, except Puerto Rico.
+        coalesce(total_specimens, 0) as total_specimens, 
+        coalesce(total_a + total_b, 0) as total_positive
+    from clinical_labs
+    join cdc_dates using (year, week)
+    where region <> 'New York City' -- always null
+    union select 
+        date, 
+        region as state, 
+        -- Most states report nulls instead of 0 when they have no specimens, except Virgin Islands.
+        coalesce(total_specimens, 0) as total_specimens, 
+        coalesce(a_h1n1 + a_h1 + a_h3 + a_no_subtype + a_unable_to_subtype + b + h3n2v, 0) as total_positive
+    from combined_labs
+    join cdc_dates using (year, week)
+    where region <> 'New York City' -- always null
+), patients as (
+    -- ILINet reports New York City separately.
+    select 
+        date, 
+        case region when 'New York City' then 'New York' else region end as state, 
+        ili_total, 
+        num_providers, 
+        total_patients
+    from ilinet_visits
+    join cdc_dates using (year, week)
+), cases as (
+    -- Convert cumulative cases/deaths to weekly new cases/deaths
+    select 
+        date, 
+        state, 
+        cases - coalesce(lag(cases) over (partition by state order by date), 0) as cases, 
+        deaths - coalesce(lag(deaths) over (partition by state order by date), 0) as deaths
+    from nyt_cases
+    where date in (select date from cdc_dates)
+)
+select 
+    date as "Date", 
+    state as "State", 
+    total_specimens as "Total Flu Tests", 
+    total_positive as "Positive Flu Tests", 
+    total_patients as "Total Patients",
+    ili_total as "Patients with ILI", 
+    num_providers as "ILINet Providers", 
+    coalesce(cases, 0) as "COVID Cases", 
+    coalesce(deaths, 0) as "COVID Deaths"
+from tests 
+join patients using (date, state)
+left join cases using (date, state)
+order by state, date;
+
+copy combined to './results/Combined.csv' with header;
