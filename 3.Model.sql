5e93f0391f721047956d20a76a0cc2fe9970fd97
George Fraser <george@fivetran.com>
2020-04-25 10:29:47 -0700

    Regionalize model

--- 3.Model.sql
+++ 3.Model.sql
@@ -1,34 +1,37 @@
 -- Fit the model ili_rate ~ seasonal_trend + b * flu_positive_rate
-create temp function rate(x int64, y int64) as (
-    case y when 0 then 0 else x / y end
-);
 create or replace table covid.features as 
 select 
     date,
-    sum(total_specimens) as total_specimens,
-    sum(total_positive) as total_positive,
-    sum(num_providers) as num_providers,
-    sum(total_patients) as total_patients,
-    sum(ili_total) as ili_total,
-    sum(population) as population,
-    coalesce(sum(cases), 0) as cases,
-    coalesce(sum(deaths), 0) as deaths,
-    -- Population-weighted average of % of patients visiting primary care providers with ILI.
-    sum(rate(ili_total, total_patients) * population) / sum(population) as ili_per_patient,
-    -- Population-weighted average of % of specimens that test positive for flu.
-    sum(rate(total_positive, total_specimens) * population) / sum(population) as positive_per_specimen,
-    -- Categorical variable for seasonal trend.
+    region,
+    total_specimens,
+    total_positive,
+    num_providers,
+    total_patients,
+    ili_total,
+    population,
+    coalesce(cases, 0) as cases,
+    coalesce(deaths, 0) as deaths,
+    ili_total / total_patients as ili_per_patient,
+    total_positive / total_specimens positive_per_specimen,
     format('Month %d', extract(month from date)) as seasonal_trend,
 from covid.patients
 join covid.tests using (date, region)
 join covid.census_population_by_region using (date, region)
 left join covid.new_cases_by_region using (date, region)
-group by date
-having num_providers > 0
-order by date;
+where total_patients > 0 and total_specimens > 0
+order by region, date;
 
 create or replace model covid.national_model
 options (model_type = 'linear_reg', input_label_cols = ['ili_per_patient']) as
-select ili_per_patient, positive_per_specimen, seasonal_trend
+select
+    ili_per_patient,
+    positive_per_specimen,
+    region,
+    seasonal_trend
 from covid.features
 where extract(year from date) <> 2020;
+
+-- Evaluate the model for making charts.
+create or replace table covid.prediction
+select *
+from ml.predict(model `fivetran-covid.covid.national_model`, table `fivetran-covid.covid.features`);
--- 4.Evaluate.sql
+++ 4.Evaluate.sql
@@ -1,6 +1,6 @@
 
 -- Variables to use in extrapolating COVID cases from ILI data.
-declare dr_visits_per_week, ili_baseline, h1n1_visits, h1n1_cases, detection_ratio, confidence_interval float64;
+declare dr_visits_per_week, ili_baseline, h1n1_visits, h1n1_cases, detection_ratio float64;
 
 -- How many times does the average American visit a primary-care provider each week?
 -- We use the numbers from the CDC's web site https://www.cdc.gov/nchs/fastats/physician-visits.htm
@@ -26,26 +26,17 @@ set h1n1_cases = 60.8 * 1000 * 1000;
 -- We will assume the same % of Americans with COVID visit their doctor.
 set detection_ratio = h1n1_visits / h1n1_cases;
 
--- 95% confidence interval for predicted ILI.
--- We'll use this to calculate "excess ILI", which is our estimate of how many people with COVID are visiting a PCP.
-set confidence_interval = (select 2*mean_absolute_error from ml.evaluate(model `fivetran-covid.covid.national_model`));
-
 -- Estimate the total number of cases.
 select 
     date,
+    region,
     ili_per_patient,
     predicted_ili_per_patient,
-    predicted_ili_per_patient + confidence_interval as excess_ili_threshold,
     dr_visits_per_week,
     population,
     detection_ratio,
     cases,
-    deaths,
-    (ili_per_patient - predicted_ili_per_patient - confidence_interval) as excess_ili
+    deaths
 from ml.predict(model `fivetran-covid.covid.national_model`, table `fivetran-covid.covid.features`)
 where date >= '2020-03-01'
-order by date;
-
--- Evaluate the model for making charts.
-select * except (seasonal_trend)
-from ml.predict(model `fivetran-covid.covid.national_model`, table `fivetran-covid.covid.features`);
+order by region, date;
